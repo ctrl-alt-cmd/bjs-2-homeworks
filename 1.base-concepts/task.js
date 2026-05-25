354f4eb785b4bffd68e5ceae59156c282aacb256
ctrl-alt-cmd <idontgiveashita@gmail.com>
2026-05-25 21:56:38 +0300

    первое задание2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  const d = Math.pow(b, 2) - 4 * a * c;
+  let d = Math.pow(b, 2) - 4 * a * c;
   if (d < 0) {
   } else if (d === 0) {
     arr.push(-b / (2 * a));
