bd0ef3d729ddfdd6dda0ebdfad69f04e6eca070c
ctrl-alt-cmd <idontgiveashita@gmail.com>
2026-06-19 20:07:26 +0300

    classes task 1-2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -33,8 +33,6 @@ let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
-student1.addMarks(5);
-student1.addMarks(7);
 console.log(student1.getAverage()); // 4.5
 console.log(student1);
 // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
