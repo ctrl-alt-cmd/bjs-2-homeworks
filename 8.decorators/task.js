f283898ef3f26d2982963bf1dacc7cc1d7e17fc3
ctrl-alt-cmd <idontgiveashita@gmail.com>
2026-07-29 21:10:39 +0300

    exception 3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,84 @@
+const md5 = require("blueimp-md5");
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      // если элемент найден
+      console.log("Из кеша: " + objectInCache.result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кеша: " + objectInCache.result;
+    }
+    let result = func(...args); // в кеше результата нет — придётся считать
+    cache.push({ hash, result }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый)
+    }
+    console.log("Вычисляем: " + result);
+    console.log(cache);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
 
+const addAndMultiply = (a, b, c) => (a + b) * c;
+const upgraded = cachingDecoratorNew(addAndMultiply);
+upgraded(1, 2, 3); // вычисляем: 9
+upgraded(1, 2, 3); // из кеша: 9
+upgraded(2, 2, 3); // вычисляем: 12
+upgraded(3, 2, 3); // вычисляем: 15
+upgraded(4, 2, 3); // вычисляем: 18
+upgraded(5, 2, 3); // вычисляем: 21
+upgraded(6, 2, 3); // вычисляем: 24 (при этом кеш для 1, 2, 3 уничтожается)
+upgraded(1, 2, 3); // вычисляем: 9  (снова вычисляем, кеша нет)
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+//function debounceDecoratorNew(func, delay) {}
+
+function debounceDecoratorNew(func, ms) {
+  let timeout = null;
+  let isCooldown = false;
+
+  function wrapper(...args) {
+    // Первый вызов — сразу
+    if (!isCooldown) {
+      func.apply(this, args);
+      wrapper.count++;
+
+      isCooldown = true;
+
+      timeout = setTimeout(() => {
+        isCooldown = false;
+        timeout = null;
+      }, ms);
+
+      return;
+    }
+
+    // Все последующие вызовы считаем
+    wrapper.allCount++;
+
+    // Отменяем предыдущий отложенный вызов
+    clearTimeout(timeout);
+
+    // Планируем новый
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+
+      // После выполнения снова начинается период ожидания
+      isCooldown = true;
+
+      timeout = setTimeout(() => {
+        isCooldown = false;
+        timeout = null;
+      }, ms);
+    }, ms);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
